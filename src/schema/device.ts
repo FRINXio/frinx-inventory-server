4fe1bc38ee33c0b4d2344a552b7ebf3fedd6337c
Pavol Porubsky <paulooze@gmail.com>
2022-08-10 13:04:47 +0200

    remove unused variable

--- src/schema/device.ts
+++ src/schema/device.ts
@@ -151,7 +151,7 @@ export const DevicesQuery = extendType({
         filter: FilterDevicesInput,
         orderBy: DeviceOrderByInput,
       },
-      resolve: async (_, args, { prisma, tenantId, arangoClient }) => {
+      resolve: async (_, args, { prisma, tenantId }) => {
         const { filter, orderBy } = args;
         const labels = filter?.labels ?? [];
         const dbLabels = await prisma.label.findMany({ where: { name: { in: labels } } });
