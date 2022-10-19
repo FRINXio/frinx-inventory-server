9ee6921ec0557aac40afa69f0f1933eae6a01c48
MarcoMruz <marcomruz1@gmail.com>
2022-10-19 14:34:02 +0200

    remove migration files

--- prisma/migrations/20221017110148_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `device_size` on the `device_inventory` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "device_inventory" DROP COLUMN "device_size";
