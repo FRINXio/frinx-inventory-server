2be70106f69ef1d71eb69e2e7b6370c6665166de
Martin Sottnik <msottnik@gmail.com>
2022-10-25 13:56:49 +0200

    search device by name fix (#207)

--- src/helpers/device-helpers.ts
+++ src/helpers/device-helpers.ts
@@ -23,7 +23,7 @@ function getLabelsQuery(labelIds: string[]): Record<string, unknown> | undefined
   return labelIds.length ? { some: { labelId: { in: labelIds } } } : undefined;
 }
 function getDeviceNameQuery(deviceName?: string | null): Record<string, unknown> | undefined {
-  return deviceName ? { startsWith: deviceName } : undefined;
+  return deviceName ? { contains: deviceName } : undefined;
 }
 export function getFilterQuery(filter?: FilterInput | null): FilterQuery | undefined {
   if (!filter) {
