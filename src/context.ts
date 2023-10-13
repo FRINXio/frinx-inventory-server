391c06ba9af7fefc8fcedc51373e2e99ca10f0f0
Pavol Porubsky <paulooze@gmail.com>
2023-10-13 11:13:06 +0200

    fix tenantId in context

--- src/context.ts
+++ src/context.ts
@@ -1,5 +1,6 @@
 import { ExpressContextFunctionArgument } from '@apollo/server/express4';
 import { PrismaClient } from '@prisma/client';
+import { IncomingHttpHeaders } from 'http';
 import topologyDiscoveryAPI, { TopologyDiscoveryAPI } from './external-api/topology-discovery';
 import getTopologyDiscoveryApi, { TopologyDiscoveryGraphQLAPI } from './external-api/topology-discovery-graphql';
 import uniconfigAPI, { UniConfigAPI } from './external-api/uniconfig';
@@ -13,16 +14,20 @@ export type Context = {
   topologyDiscoveryGraphQLAPI?: TopologyDiscoveryGraphQLAPI;
 };
 
+function getTenantIdFromHeaders(headers: IncomingHttpHeaders): string {
+  if (headers['x-tenant-id'] == null) {
+    return 'frinx';
+  }
+  if (Array.isArray(headers['x-tenant-id'])) {
+    return headers['x-tenant-id'][0];
+  }
+  return headers['x-tenant-id'];
+}
+
 export default async function createContext(context: ExpressContextFunctionArgument): Promise<Context> {
   const { req } = context;
   const { headers } = req;
-  let tenantId: string;
-  if (headers['x-tenant-id'] == null) {
-    // throw new Error('tenant id is missing');
-    tenantId = 'frinx';
-  } else {
-    tenantId = headers['x-tenant-id'] as string;
-  }
+  const tenantId = getTenantIdFromHeaders(headers);
 
   return {
     prisma: prismaClient,
